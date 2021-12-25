d925aea78e0ada4e503d09fcd85d7fe2508344ef
Łukasz Grolik <lukasz@grolik.pl>
2021-12-25 04:29:35 +0100

    Quad-tree impl & UT update

--- src/lib/geom/quad-tree.ts
+++ src/lib/geom/quad-tree.ts
@@ -1,16 +1,46 @@
 import { Vector2 } from "./vec2";
 
+export function rectanglesIntersect(box1: QuadTreeObject, box2: QuadTreeObject): boolean {
+    const box1Pos = box1.getPosition();
+    const box1Size = box1.getSize();
+    const box2Pos = box2.getPosition();
+
+    if (box2Pos.x > box1Pos.x + box1Size.x) return false;
+
+    const box2Size = box2.getSize();
+
+    if (box2Pos.x + box2Size.x < box1Pos.x) return false;
+    if (box2Pos.y + box2Size.y < box1Pos.y) return false;
+    if (box2Pos.y > box1Pos.y + box1Size.y) return false;
+
+    return true;
+}
+
 class Quad {
 
 }
 
-interface AddObjectBody<T> {
-    obj: T;
-    pos: Vector2;
-    size: Vector2;
+export interface QuadTreeObject {
+    getPosition(): Vector2;
+    getSize(): Vector2;
 }
 
-export class QuadTree<T> {
+// interface AddObjectBody {
+//     obj: QuadTreeObject;
+//     pos: Vector2;
+//     size: Vector2;
+// }
+
+type GridOpts = {
+    gridSize: Vector2;
+    cellSize: Vector2;
+};
+type QuadTreeOpts<T> = GridOpts & {
+    onCollisionEnter?: (obj: T) => void;
+    onCollisionExit?: (obj: T) => void;
+};
+
+export class QuadTree<T extends QuadTreeObject> {
     private gridSize: Vector2;
     private cellSize: Vector2;
 
@@ -21,40 +51,119 @@ export class QuadTree<T> {
     readonly quadsObjectsMap = new Map<Quad, T[]>();
     readonly objectsObjectsMap = new Map<T, T[]>();
 
-    constructor() {
-        this.gridSize = new Vector2(10, 10);
-        this.cellSize = new Vector2(10, 10);
+    constructor(opts: QuadTreeOpts<T>) {
+        this.gridSize = opts.gridSize
+        this.cellSize = opts.cellSize;
+
+        this.quads = new Array(this.gridSize.x * this.gridSize.y).fill(undefined);
+        for (let i = 0; i < this.quads.length; i++) {
+            const quad = new Quad();
+            this.quads[i] = quad;
+            this.quadsObjectsMap.set(quad, []);
+        }
     }
 
-    public setGridSize(size: Vector2): void {
+    // public setGridSize(size: Vector2): void {
+
+    // }
+
+    // public setCellSize(cellSize: Vector2): void {
+
+    // }
+
+    public updateGrid(opts: GridOpts) {
 
     }
 
-    public setCellSize(cellSize: Vector2): void {
+    // public addObject(obj: T): void {
+    //     this.objects.push(obj);
+    // }
 
+    public addObjects(objs: T[]): void {
+        this.objects.push(...objs);
+
+        for (let i = 0; i < objs.length; i++) {
+            const obj = objs[i];
+
+            // @todo divide quad tree if necessary
+            // @todo find which quads object collides with
+            // @todo add object to quads
+            // @todo add quads to object
+        }
+
+        // @todo call onCollisionEnter/Exit (max once for each obj)
     }
 
-    public addObject(obj: AddObjectBody<T>): void {
+    // public removeObject(obj: T): void {
+    //     const index = this.objects.indexOf(obj);
 
-    }
-
-    public addObjects(objs: AddObjectBody<T>[]): void {
-
-    }
-
-    public removeObject(obj: T): void {
-
-    }
+    //     if (index !== -1) {
+    //         this.objects.splice(index, 1);
+    //     }
+    // }
 
     public removeObjects(objs: T[]): void {
+        for (let i = 0; i < objs.length; i++) {
+            const obj = objs[i];
 
+            // this.removeObject(obj);
+            const index = this.objects.indexOf(obj);
+            if (index !== -1) {
+                this.objects.splice(index, 1);
+            }
+
+            // @todo remove object from quads
+        }
+
+        // @todo call onCollisionEnter/Exit (max once for each obj)
     }
 
     public setObjectPosition(obj: T, pos: Vector2): void {
+        // @todo find and update object
+        // @todo find which quads object collides with after update
+        // @todo update object quads
+        // @todo update quads objects
 
+        // @todo call onCollisionEnter/Exit
     }
 
     public setObjectSize(obj: T, pos: Vector2): void {
+        // @todo find and update object
+        // @todo find which quads object collides with after update
+        // @todo update object quads
+        // @todo update quads objects
 
+        // @todo call onCollisionEnter/Exit
+    }
+
+    public updateObjects(objs: T[]): void {
+
+    }
+
+    public findCollisions(): T[] {
+        const collisions: T[] = [];
+
+        for (let i = 0; i < this.objects.length; i++) {
+            const objA = this.objects[i];
+
+            for (let j = 0; j < this.objects.length; j++) {
+                const objB = this.objects[j];
+                if (objA === objB) continue;
+                if (collisions.includes(objA) && collisions.includes(objB)) continue;
+
+                if (rectanglesIntersect(objA, objB)) {
+                    collisions.push(objA, objB);
+                }
+            }
+        }
+
+        return collisions;
+    }
+
+    // @todo perf test
+    public findCollisionsByQuads(): T[] {
+        const collisions: T[] = [];
+
+        return collisions;
     }
 }
--- src/lib/geom/test/quad-tree.test.ts
+++ src/lib/geom/test/quad-tree.test.ts
@@ -4,26 +4,112 @@ import { QuadTree } from '../quad-tree';
 import { Vector2 } from '../vec2';
 
 describe('quad-tree', () => {
-    it('test', () => {
-        const items = [
-            {id: 1, pos: new Vector2(0, 0), size: new Vector2(1, 1)},
-            {id: 2, pos: new Vector2(12, 10), size: new Vector2(5, 5)},
-            {id: 3, pos: new Vector2(15, 10), size: new Vector2(5, 5)},
-        ]
-        const qt = new QuadTree();
-        qt.addObjects(items.map(item => {
-            return {
-                obj: item.id,
-                pos: item.pos,
-                size: item.size,
+    beforeEach(() => {
+
+    });
+
+    describe('addObjects', () => {
+        it('test', () => {
+            type Item = {
+                id: number;
+                pos: Vector2;
+                size: Vector2;
+                getPosition(): Vector2;
+                getSize(): Vector2;
             };
-        }));
 
-        should(qt.objects).deepEqual([1, 2, 3]);
-        should(qt.objectsQuadsMap.get(1)).deepEqual([])
-        should(qt.objectsObjectsMap.get(1)).deepEqual([])
+            const items: Item[] = [
+                {id: 1, pos: new Vector2(0, 0), size: new Vector2(1, 1)},
+                {id: 2, pos: new Vector2(12, 10), size: new Vector2(5, 5)},
+                {id: 3, pos: new Vector2(15, 10), size: new Vector2(5, 5)},
+            ].map(item => {
+                return {
+                    id: item.id,
+                    pos: item.pos,
+                    size: item.size,
+                    getPosition() {
+                        return this.pos;
+                    },
+                    getSize() {
+                        return this.size;
+                    }
+                };
+            });
+            const qt = new QuadTree<Item>({
+                gridSize: new Vector2(10, 10),
+                cellSize: new Vector2(10, 10),
+            });
+            qt.addObjects(items);
 
-        should(qt.objectsObjectsMap.get(2)).deepEqual([3])
-        should(qt.objectsObjectsMap.get(3)).deepEqual([2])
+            should(qt.objects.map(o => o.id)).deepEqual([1, 2, 3]);
+            should(qt.objectsQuadsMap.get(items[0])).deepEqual([])
+            should(qt.objectsObjectsMap.get(items[0])).deepEqual([])
+
+            should(qt.objectsObjectsMap.get(items[1])).deepEqual([3])
+            should(qt.objectsObjectsMap.get(items[2])).deepEqual([2])
+        });
+
+        it('', () => {
+
+        });
+    });
+
+    describe('removeObjects', () => {
+
+    });
+
+    describe('updateObjects', () => {
+
+    });
+
+    describe('findCollisions', () => {
+        it('test', () => {
+            type Item = {
+                id: number;
+                pos: Vector2;
+                size: Vector2;
+                getPosition(): Vector2;
+                getSize(): Vector2;
+            };
+
+            const items: Item[] = [
+                { id: 1, pos: new Vector2(0, 0), size: new Vector2(1, 1) },
+                { id: 2, pos: new Vector2(12, 10), size: new Vector2(5, 5) },
+                { id: 3, pos: new Vector2(15, 10), size: new Vector2(5, 5) },
+            ].map(item => {
+                return {
+                    id: item.id,
+                    pos: item.pos,
+                    size: item.size,
+                    getPosition() {
+                        return this.pos;
+                    },
+                    getSize() {
+                        return this.size;
+                    }
+                };
+            });
+            const qt = new QuadTree<Item>({
+                gridSize: new Vector2(10, 10),
+                cellSize: new Vector2(10, 10),
+            });
+            qt.addObjects(items);
+
+            const collisions = qt.findCollisions();
+
+            should(collisions.length).equal(2);
+            should(collisions.map(c => c.id)).deepEqual([2, 3]);
+
+            // should(qt.objects.map(o => o.id)).deepEqual([1, 2, 3]);
+            // should(qt.objectsQuadsMap.get(items[0])).deepEqual([])
+            // should(qt.objectsObjectsMap.get(items[0])).deepEqual([])
+
+            // should(qt.objectsObjectsMap.get(items[1])).deepEqual([3])
+            // should(qt.objectsObjectsMap.get(items[2])).deepEqual([2])
+        });
+    });
+
+    describe('findCollisionsByQuads', () => {
+
     });
 });
