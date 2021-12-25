9490b80a59a2818cb92510747af89cbcf5485d8e
Łukasz Grolik <lukasz@grolik.pl>
2021-12-25 05:31:11 +0100

    Procgen SVG weave pattern

--- src/views/procgen-svg-view/procgen-svg-view.tsx
+++ src/views/procgen-svg-view/procgen-svg-view.tsx
@@ -46,8 +46,147 @@ export const ProcgenSvgView: React.FC<{ store: Store.Store }> = observer(({ stor
 
     }, []);
 
+    const randomLayers = random.samples(_.arr(20), 20);
+
     return (
         <Wrapper>
+            <svg
+                width={1800}
+                height={1000}
+                // width="100%"
+                viewBox={`0 0 ${1800} ${1000}`}
+                version="1.1"
+                xmlns="http://www.w3.org/2000/svg"
+                style={{ backgroundColor: '#eee' }}
+            >
+                <g>
+                    {
+                        _.arr(10).map(i => {
+                            const height = 20;
+                            const hue = random.range(0, 360);
+
+                            return (
+                                <rect
+                                    x={0}
+                                    y={i * height}
+                                    width={200}
+                                    height={height}
+                                    strokeWidth={0}
+                                    fill={`hsl(${hue}, 50%, 50%)`}
+                                    transform={random.value() > .5 ? `rotate(90 100 100)` : ''}
+                                    style={{
+                                        // position: 'absolute',
+                                        // zIndex: randomLayers.slice(0, 10)[i],
+                                    }}
+                                />
+                            )
+                        })
+                    }
+                </g>
+
+                <g transform={`translate(250 0)`}>
+                    {
+                        _.arr(20).map(i => {
+                            const height = 20;
+                            const hue = random.range(0, 360);
+
+                            return (
+                                <rect
+                                    x={0}
+                                    y={i * height}
+                                    width={400}
+                                    height={height}
+                                    strokeWidth={0}
+                                    fill={`hsl(${hue}, 50%, 50%)`}
+                                    transform={random.value() > .5 ? `rotate(90 200 200)` : ''}
+                                    style={{
+                                        // position: 'absolute',
+                                        // zIndex: randomLayers.slice(0, 10)[i],
+                                    }}
+                                />
+                            )
+                        })
+                    }
+                </g>
+
+                <g transform={`translate(700 0)`}>
+                    {
+                        _.arr(100).map(i => {
+                            const height = 20;
+                            const hue = random.range(0, 360);
+
+                            return (
+                                <rect
+                                    x={0}
+                                    y={random.range(0, 20 - 1) * height}
+                                    width={400}
+                                    height={height}
+                                    strokeWidth={0}
+                                    fill={`hsl(${hue}, 50%, 50%)`}
+                                    transform={random.value() > .5 ? `rotate(90 200 200)` : ''}
+                                    style={{
+                                        // position: 'absolute',
+                                        // zIndex: randomLayers.slice(0, 10)[i],
+                                    }}
+                                />
+                            )
+                        })
+                    }
+                </g>
+
+                <g transform={`translate(1150 0)`}>
+                    {
+                        _.arr(100).map(i => {
+                            const height = 20;
+                            const hue = random.range(0, 360);
+
+                            return (
+                                <rect
+                                    x={0}
+                                    y={random.range(0, 20 - 1) * height}
+                                    width={400}
+                                    height={height}
+                                    strokeWidth={0}
+                                    // fill={`hsl(${hue}, 50%, 50%)`}
+                                    fill={random.sample(planet.colors)}
+                                    transform={random.value() > .5 ? `rotate(90 200 200)` : ''}
+                                    style={{
+                                        // position: 'absolute',
+                                        // zIndex: randomLayers.slice(0, 10)[i],
+                                    }}
+                                />
+                            )
+                        })
+                    }
+                </g>
+
+                <g transform={`translate(100 500) rotate(45 200 200)`}>
+                    {
+                        _.arr(100).map(i => {
+                            const height = 20;
+                            const hue = random.range(0, 360);
+
+                            return (
+                                <rect
+                                    x={0}
+                                    y={random.range(0, 20 - 1) * height}
+                                    width={400}
+                                    height={height}
+                                    strokeWidth={0}
+                                    // fill={`hsl(${hue}, 50%, 50%)`}
+                                    fill={random.sample(planet.colors)}
+                                    transform={random.value() > .5 ? `rotate(90 200 200)` : ''}
+                                    style={{
+                                        // position: 'absolute',
+                                        // zIndex: randomLayers.slice(0, 10)[i],
+                                    }}
+                                />
+                            )
+                        })
+                    }
+                </g>
+            </svg>
+
             <svg
                 width={WIDTH}
                 height={HEIGHT}
